d616a756a6d1bb4385471a2602744128799c55c3
Oc1S <ct1147564685@gmail.com>
2022-11-28 02:42:53 +0800

    fix: download script

--- download.sh
+++ download.sh
@@ -1,4 +1,7 @@
-cd apis/vqa/data
+cd apis/vqa
+if [ ! -d 'data' ]; then
+    mkdir data
+fi
 wget -c https://xaneon.oss-cn-hangzhou.aliyuncs.com/data/vocab_file2.pkl
 wget -c https://xaneon.oss-cn-hangzhou.aliyuncs.com/data/vgg16.tfmodel
 if [ ! -d 'models' ]; then
--- vqa_system/urls.py
+++ vqa_system/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from apis import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path("apis/", include("apis.urls")),
     path("admin/", admin.site.urls),
 ]
